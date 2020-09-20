c6f8833ce02ed439bc7be4cd4dc1e25d762d3c01
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-09-20 13:50:10 +0100

    Working game.

---
+++ NonoBlazor/Shared/StandardNonogramCell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NonoBlazor.Shared
+{
+    public class StandardNonogramCell
+    {
+        public bool MarkedFilled { get; set; } = false;
+        public bool MarkedEmpty { get; set; } = false;
+
+        public void FlipMarkedFilled()
+        {
+            if (!MarkedEmpty)
+                MarkedFilled = !MarkedFilled;
+        }
+
+        public void FlipMarkedEmpty()
+        {
+            if (!MarkedFilled)
+                MarkedEmpty = !MarkedEmpty;
+        }
+    }
+}
